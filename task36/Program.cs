62088e8beea6b757e7d044080f1c871581166db9
Evgeniya <prahova.e@ya.ru>
2023-02-17 01:29:39 +0500

    All tasks

---
+++ task36/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+int SumNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        result = result + array[i];
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int[] array = GenerateArray(7, -10, 10);
+PrintArray(array);
+int sum = SumNumbers(array);
+System.Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях: {SumNumbers(array)}");
+
+
---
+++ task38/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] GenerateArray(int size)
+{
+    double[] array = new double[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(rand.NextDouble() * 100, 2);
+    }
+    return array;
+}
+
+double difference(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    double result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+        result = Math.Round(max - min, 2);
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+double[] array = GenerateArray(5);
+PrintArray(array);
+double diff = difference(array);
+System.Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {difference(array)}");
+
